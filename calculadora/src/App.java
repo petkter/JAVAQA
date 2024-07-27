f3b7a7b6d4d154526220cf289fd917a293ba8ed6
petkter <luispetteralvarado@gmail.com>
2024-07-27 01:04:49 -0300

    angelesgei2

--- calculadora/src/App.java
+++ calculadora/src/App.java
@@ -4,8 +4,9 @@ public class App {
         Scanner mmgvo = new Scanner(System.in);
         System.out.println("Bienvenido a la calculadora ");
         int op = 0;
-        int numerouno;
-        int numerodos;
+        int numerouno = 0;
+        int numerodos = 0;
+        int resultado = 0; 
         System.out.println("Elija una opcion de calculo ");
         System.out.println("Digite el numero 1 para adicion ");
         System.out.println("Digite el numero 2 para substracion ");
@@ -14,14 +15,25 @@ public class App {
         do {
         switch (op) {
             case 1: 
-                
+                System.out.println("Digite dos numeros: ");
+                numerouno = mmgvo.nextInt();
+                numerodos = mmgvo.nextInt();
+                resultado = numerouno + numerodos; 
+                System.out.println("Resultado " + resultado);
+                op = 3;
                 break;
             case 2:
+                System.out.println("Digite dos numero: ");
+                numerouno = mmgvo.nextInt();
+                numerodos = mmgvo.nextInt();
+                resultado = numerouno - numerodos;
+                System.out.println("Resultado: " + resultado);
+                op = 3;
                 break;
             default:
                 break;
         }
             
-        } while (!=0);
+        } while (op!=3);
     }
 }
